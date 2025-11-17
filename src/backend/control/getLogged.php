9cf89b7445a415f7310814f3ee3e77080264a1b3
Rynilan <ilanvitorsantos@gmail.com>
2025-11-17 12:50:13 -0300

    feat: criado o arquivo para carregar sessões.
    code: pegar as informações de quem está logado fora somente se está.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- src/backend/control/getLogged.php
+++ src/backend/control/getLogged.php
@@ -1,5 +1,8 @@
 <?php
-
 require_once 'loadSession.php';
-echo json_encode(['logged' => $_SESSION['LOGGED']]);
+echo json_encode([
+	'logged' => $_SESSION['LOGGED'],
+	'mail' => $_SESSION['USER_MAIL'],
+	'name' => $_SESSION['USER_NAME']
+]);
 ?>
