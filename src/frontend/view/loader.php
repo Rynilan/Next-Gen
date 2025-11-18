0d68e879526697b1c721201058c4d2210df5bc53
Rynilan <ilanvitorsantos@gmail.com>
2025-11-18 13:46:48 -0300

    code: adicionada lógica de nível de acesso, logado ou não.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- src/frontend/view/loader.php
+++ src/frontend/view/loader.php
@@ -4,11 +4,24 @@ $template = file_get_contents('html/template.html');
 $page = $_GET['page_name'];
 $content = '';
 
+// Checa se a página requere login.
+require_once '../../backend/control/loadSession.php';
+$logged_pages = json_decode(file_get_contents('../../../assets/data/app/needLogged.json'), true)['true'];
+if (in_array($page, $logged_pages) && !$_SESSION['LOGGED']) {
+	require_once '../../backend/control/loadEnv.php';
+	http_response_code(401);
+	header('Location: '.$_ENV['ROOT_PATH'].'frontend/view/loader.php?page_name=error&code_error=401');
+	exit();
+}
+
+// Pega o conteúdo html.
 if ($page === 'error') {
 	$content = file_get_contents('html/errors/'.basename($_GET['code_error']).'.html');
 } else {
 	$content = file_get_contents('html/pages/'.basename($page).'.html');
 }
+
+// Verifica se a página existe.
 if ($content === false) {
 	require_once '../../backend/control/loadEnv.php';
 	http_response_code(404);
@@ -16,8 +29,8 @@ if ($content === false) {
 	exit();
 }
 
+// Carrega a página.
 $finalPage = str_replace('<!-- Main Content -->', $content, $template);
-
 echo $finalPage;
 
 ?>
