2d5b0019e3f8ede7153b3f57dcd78e2e59f7d315
Rynilan <ilanvitorsantos@gmail.com>
2025-12-25 01:35:13 -0300

    code: ajustado o index.
    Signed-off-by: Rynilan <ilanvitorsantos@gmail.com>

--- index.php
+++ index.php
@@ -2,9 +2,5 @@
 include 'src/backend/control/utils/redirect.php';
 require_once 'src/backend/control/utils/loadSession.php';
 
-$_SESSION['USER_MAIL'] = null;
-$_SESSION['USER_NAME'] = null;
-$_SESSION['LOGGED'] = false;
-
-redirect('login');
+redirect('home');
 ?>
